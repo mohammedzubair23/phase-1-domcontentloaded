dfcfaf9e5218d0f582ac2195536a254f85fb4d65
Mohammed Zubair <mohammedzubair10@yahoo.com>
2022-01-17 17:57:41 -0500

    commit tree 31f270c68d49f9a1dac9c54e0b28655c3d45a128

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", domContentLoadedEventHandler);
+
+function domContentLoadedEventHandler() {
+   let textElement = document.getElementById("text") 
+    textElement.innerHTML = "This is really cool!"
+}
+
